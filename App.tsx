219ef746ac8e0e444593ee8919b3c55c1ea4684e
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-04-30 18:12:08 -0300

    feat: Added dashboard

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-    </View>
-  );
+  return <Dashboard />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
