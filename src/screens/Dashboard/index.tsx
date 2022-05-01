829f9cee4509a02e8c094ad82900895b23fb1b0a
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-04-30 22:07:19 -0300

    feat: added header to dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
-import { Container, Title } from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  Username,
+  Icon
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://github.com/pedrobovolindavila.png' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <Username>Pedro</Username>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
