2051a594f21bb7d464256b0d09c5e2aabab1fdcc
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-01 02:58:44 -0300

    feat: added category select component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Input } from "../../components/Forms/Input";
 import { Button } from "../../components/Forms/Button";
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
+import { CategorySelect } from "../../components/Forms/CategorySelect";
 
 import {
   Container,
@@ -53,6 +54,10 @@ export function Register() {
               active={transactionType === 'down'}
             />
           </TransactionsTypes>
+
+          <CategorySelect
+            title="Categoria"
+          />
         </Fields>
 
         <Button title="Enviar" activeOpacity={.7} />
