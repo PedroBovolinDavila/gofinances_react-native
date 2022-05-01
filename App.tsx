d14217baa5a7448568c7b94c0b166002dae6ac51
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-01 14:20:24 -0300

    feat: added bottom-tab navigation to app

--- App.tsx
+++ App.tsx
@@ -9,9 +9,10 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import { Register } from './src/screens/Register';
-
 import theme from './src/global/styles/theme';
+
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
