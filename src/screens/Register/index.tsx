dc0c24a0641f3dc0c8611c94ec259ec43c010792
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-01 12:40:07 -0300

    fix: fixed keyboard bugs

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from 'react-hook-form'
-import { Modal } from 'react-native'
+import { Keyboard, Modal, TouchableWithoutFeedback } from 'react-native'
 
 import { InputForm } from "../../components/Forms/InputForm";
-import { Input } from "../../components/Forms/Input";
 import { Button } from "../../components/Forms/Button";
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/Forms/CategorySelectButton";
@@ -60,64 +59,61 @@ export function Register() {
   }
 
   return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-
-      <Form>
-        <Fields>
-          <InputForm
-            name="name"
-            control={control}
-            placeholder="Nome"
-          />
-
-          <InputForm
-            name="amount"
-            control={control}
-            placeholder="Preço"
-          />
-
-          <TransactionsTypes>
-            <TransactionTypeButton
-              activeOpacity={.5}
-              type="up"
-              title="Entrada"
-              onPress={() => handleTransactionTypeSelect('up')}
-              active={transactionType === 'up'}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+        <Form>
+          <Fields>
+            <InputForm
+              name="name"
+              control={control}
+              placeholder="Nome"
+              autoCapitalize="sentences"
+              autoCorrect={false}
             />
-
-            <TransactionTypeButton
-              activeOpacity={.5}
-              type="down"
-              title="Saida"
-              onPress={() => handleTransactionTypeSelect('down')}
-              active={transactionType === 'down'}
+            <InputForm
+              name="amount"
+              control={control}
+              placeholder="Preço"
+              keyboardType="numeric"
             />
-          </TransactionsTypes>
-
-          <CategorySelectButton
-            onPress={handleOpenSelectCategoryModal}
-            title={category.name}
+            <TransactionsTypes>
+              <TransactionTypeButton
+                activeOpacity={.5}
+                type="up"
+                title="Entrada"
+                onPress={() => handleTransactionTypeSelect('up')}
+                active={transactionType === 'up'}
+              />
+              <TransactionTypeButton
+                activeOpacity={.5}
+                type="down"
+                title="Saida"
+                onPress={() => handleTransactionTypeSelect('down')}
+                active={transactionType === 'down'}
+              />
+            </TransactionsTypes>
+            <CategorySelectButton
+              onPress={handleOpenSelectCategoryModal}
+              title={category.name}
+            />
+          </Fields>
+          <Button
+            title="Enviar"
+            activeOpacity={.7}
+            onPress={handleSubmit(handleRegister)}
           />
-        </Fields>
-
-        <Button
-          title="Enviar"
-          activeOpacity={.7}
-          onPress={handleSubmit(handleRegister)}
-        />
-      </Form>
-
-      <Modal visible={categoryModalOpen}>
-        <CategorySelect
-          category={category}
-          setCategory={setCategory}
-          closeSelectCategory={handleCloseSelectCategoryModal}
-        />
-      </Modal>
-
-    </Container>
+        </Form>
+        <Modal visible={categoryModalOpen}>
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
+          />
+        </Modal>
+      </Container>
+    </TouchableWithoutFeedback>
   )
 }
